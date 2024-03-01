27a7612597beb5a57c0a5f4c4d4800a2b3b4b16f
naso576 <70210241+naso576@users.noreply.github.com>
2024-03-01 23:25:48 +0530

    Update tablets.controller.js

--- controllers/tablets.controller.js
+++ controllers/tablets.controller.js
@@ -56,6 +56,12 @@ exports.findTabletsList =(req,res) => {
 console.log('calling')
     TabletsList.find()
     .then(data => {
+
+        res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
       res.send(data);
      
     })
