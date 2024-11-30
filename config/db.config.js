9583d3515bf1b5867e3dd79a4d337ab5d0f8bb73
naso576 <70210241+naso576@users.noreply.github.com>
2024-11-30 19:56:52 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // url: "mongodb://127.0.0.1:27017/projectnktd"
-    url : "mongodb+srv://nktdc:nktdc1234@cluster0.rycy3.mongodb.net//?retryWrites=true&w=majority&appName=nktdc-db"
+    url : "mongodb+srv://nktdc:nktdc1234@cluster0.rycy3.mongodb.net//?retryWrites=true&w=majority&appName=Cluster0"
   };
 
 
