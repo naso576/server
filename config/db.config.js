7af4a474b7c10ace485d69f30c07551472021a53
naso576 <70210241+naso576@users.noreply.github.com>
2024-12-06 20:59:42 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     // url: "mongodb://127.0.0.1:27017/projectnktd"
-    url : "mongodb+srv://projectnaveen:naso2024@cluster0.rycy3.mongodb.net//?retryWrites=true&w=majority&appName=Cluster0/projectnaveen"
-  };
+   // url : "mongodb+srv://projectnaveen:naso2024@cluster0.rycy3.mongodb.net//?retryWrites=true&w=majority&appName=Cluster0/projectnaveen"
+ url : "mongodb+srv://nktdc:nktdc1234@nktdc-db.wm7fb.mongodb.net/?retryWrites=true&w=majority&appName=nktdc-db/projectnktd"
+  
+};
 
 
 
