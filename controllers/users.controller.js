238911a51d87b46306d747c3e5bd1c460e332a72
naso576 <70210241+naso576@users.noreply.github.com>
2025-04-11 17:40:33 +0530

    Add files via upload

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -28,6 +28,13 @@ exports.createUser = (req, res) => {
   user
     .save(user)
     .then(data => {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Max-Age", "1800");
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+
       res.send(data);
       console.log("User created successfully", user);
     })
