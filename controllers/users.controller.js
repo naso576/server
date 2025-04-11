7f28ad66ed9ee4082adf746569e11bec0bfb523c
naso576 <70210241+naso576@users.noreply.github.com>
2025-04-11 18:48:18 +0530

    Update users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -28,15 +28,14 @@ exports.createUser = (req, res) => {
   user
     .save(user)
     .then(data => {
-      // res.setHeader("Access-Control-Allow-Origin", "*")
-      // res.setHeader("Access-Control-Allow-Credentials", "true");
-      // res.setHeader("Access-Control-Max-Age", "1800");
-      // res.setHeader("Access-Control-Allow-Headers", "content-type");
-      // res.setHeader("Content-Type", "application/json");
-      // res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Max-Age", 1800);
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 
       res.send(data);
-      console.log("User created successfully", user);
     })
     .catch(err => {
       if (err) {
